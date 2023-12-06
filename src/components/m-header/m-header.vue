5290e451aa03ddb6895481bc7c68f975d17ea05a
JLP <jialeipeng@outlook.com>
2023-12-06 10:38:04 +0800

    Init:初始化

--- src/components/m-header/m-header.vue
+++ src/components/m-header/m-header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="m-header">
     <div class="icon"></div>
-    <h1 class="text">冲冲-Music</h1>
+    <h1 class="text">播放器</h1>
     <router-link tag="div" class="mine" to="/user">
       <i class="icon-mine"></i>
     </router-link>
